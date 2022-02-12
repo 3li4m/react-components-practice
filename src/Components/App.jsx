8f29d1f638ac4f70586cc5bc91aaf1dbb5d18b84
ElieAjaka <elie.ajaka87@gmail.com>
2022-02-12 22:29:19 +1100

    Initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading";
+
+// Use a div in case we need to import multiple components
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
